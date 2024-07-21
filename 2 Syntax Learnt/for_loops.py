e96fbefa169ca1e9791a1e4fd12c78d7521ff105
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-07-22 02:13:56 +0530

    For Loops

--- 2 Syntax Learnt/for_loops.py
+++ 2 Syntax Learnt/for_loops.py
@@ -2,3 +2,19 @@
 for i in range(5):
     print(i)
 
+print()
+
+
+# 2
+for char in "hello":
+    print(char, end=" ")  # end=" " is used to print the output in the same line
+
+print()
+print()
+
+
+# 3
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+print()
