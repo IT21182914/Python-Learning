fe5fbdfcfcccadb6969a1c4ee98fad30f4060218
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-07-22 08:59:31 +0530

    Reverse Array

---
+++ 4 Problem Solvings/reverse_array.py
@@ -0,0 +1,14 @@
+def reverse_array(arr):
+    n = len(arr)
+    for i in range(n // 2):  # // for integer division.
+        temp = arr[i]
+        arr[i] = arr[n - 1 - i]
+        arr[n - 1 - i] = temp
+
+
+if __name__ == "__main__":
+    array = [5, 4, 2, 7, 8, 6]
+    reverse_array(array)
+    print("Reversed Array: ")
+    for j in range(len(array)):
+        print(array[j])
